a62bcb2279b8421b2a6262bc0cd29c41e9c8586f
nimeullaha71 <nimeullahacse@gmail.com>
2025-02-12 20:00:44 +0600

    dicount calculate

---
+++ First_codemama_module_11/Discount.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+ 
+void main() {
+    // Write your dart code from here
+    
+    var data = stdin.readLineSync()!.split(" ");
+
+    int a = int.parse(data[0]);
+    int b = int.parse(data[1]);
+
+    double dicountPrice = a / 100*b;
+    double price = a - dicountPrice;
+    String finalPrice = price.toStringAsFixed(2); 
+
+    print("Price: ${finalPrice}");
+    
+}
