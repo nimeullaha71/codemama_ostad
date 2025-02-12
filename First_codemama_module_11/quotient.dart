6de957dd6e902c1bd3bcc17b8a9eb72af90f5e32
nimeullaha71 <nimeullahacse@gmail.com>
2025-02-12 19:58:06 +0600

    quotient calculate

---
+++ First_codemama_module_11/quotient.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+ 
+void main() {
+    // Write your dart code from here
+    var data = stdin.readLineSync()!.split(" ");
+
+    int a = int.parse(data[0]);
+    int b = int.parse(data[1]);
+
+    int quotient = a ~/ b;
+    print(quotient);
+
+    
+}
